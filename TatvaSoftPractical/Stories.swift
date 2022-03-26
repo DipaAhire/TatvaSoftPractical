15f81c062f1b7afb20a4a9eb10f73c16c3a180a6
Dipali Ahire <ahire.dipali5@gmail.com>
2022-03-26 19:19:59 +0530

    Displayed stories data on table view using api, embedded view controller in navigation controller for displaying navigation title.

---
+++ TatvaSoftPractical/Stories.swift
@@ -0,0 +1,12 @@
+//
+//  Stories.swift
+//  TatvaSoftPractical
+//
+//  Created by Dipali Rajbanshi on 26/03/22.
+//
+
+import Foundation
+struct Stories {
+    var title: String?
+    var createdAt: String?
+}
--- TatvaSoftPractical/ViewController.swift
+++ TatvaSoftPractical/ViewController.swift
@@ -10,20 +10,25 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var tableView: UITableView!
+    var storiesData: [Stories] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Post"
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomTableViewCell")
+        stories()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return storiesData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell", for: indexPath) as? CustomTableViewCell {
+            cell.createdAt.text = storiesData[indexPath.row].createdAt
+            cell.title.text = storiesData[indexPath.row].title
             return cell
         }
         return UITableViewCell()
@@ -31,6 +36,41 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 90
+    }
+    
+    // MARK: API Calling
+    
+    func stories() {
+        let url = "https://hn.algolia.com/api/v1/search_by_date?tags=story&page=2"
+        let urlRequest = URLRequest(url: URL(string : url)!)
+        let configObj = URLSessionConfiguration.default
+        let session = URLSession(configuration: configObj)
+        let task = session.dataTask(with: urlRequest) { [weak self] (data, response, error) in
+            print(response)
+            do {
+                let outerDic = try! JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary
+                print("Outer Dic:", outerDic)
+                let dataArray = outerDic?.value(forKey: "hits") as? NSArray
+                print("DataArray:", dataArray)
+                for item in dataArray! {
+                    let dic = item as? NSDictionary
+                    let createdAt = dic?.value(forKey: "created_at") as? String
+                    let title = dic?.value(forKey: "title") as? String
+                    
+                    var storiesObj = Stories()
+                    storiesObj.createdAt = createdAt
+                    storiesObj.title = title
+                    
+                    self?.storiesData.append(storiesObj)
+                }
+                
+            }
+            
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        task.resume()
     }
     
 
