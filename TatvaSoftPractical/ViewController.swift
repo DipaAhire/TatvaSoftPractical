d94fea0578674f4e88a3cfb57bcda8bf343b4214
Dipali Ahire <ahire.dipali5@gmail.com>
2022-03-26 17:48:03 +0530

    Initial commit

---
+++ TatvaSoftPractical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TatvaSoftPractical
+//
+//  Created by Dipali Rajbanshi on 26/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
